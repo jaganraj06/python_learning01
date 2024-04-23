5e857cb6c908cc032d789bedc740e5e7fe6b4035
jaganraj06 <jaganrajmurugesan06@gmail.com>
2024-04-23 22:02:14 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+
+#printing a test
+print("welcome to python world")
+#variable
+name="abilesh"
+#printing variable and text
+print('"how are you "'+name)
+message = "happy birthday"
+name = "priyan"
+quote="god bless you"
+if(name.isupper()):
+    print("Name is upper")
+else:
+    print("Name is not upper")
+
+print(message.title())
+print(name.upper())
+print(quote+" "+name)
+print(len(message))
+print(name.find("r"))
+print(len(name))
+print(name.lower())
+print(quote*5)
+print(name.find("#"))
+print(name.replace("p","r"))
+print(message.isdigit())
+print(quote.isalpha())
+x = range(6,10,1)
+for n in x:
+    print(n)
+name,age,ward="arun",17,75
+print(name)
+print(ward)
+DOB=1.1
+DOB=str(DOB)
+print("your DOB is"+DOB)
+count=20.5
+print(float(count)+2)
+
+
+
+

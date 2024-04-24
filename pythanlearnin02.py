716ac516b2dfa9861a2662a7826cd15050f58823
jaganraj06 <jaganrajmurugesan06@gmail.com>
2024-04-24 20:35:55 +0530

    phython learning day02

---
+++ pythanlearnin02.py
@@ -0,0 +1,12 @@
+name=input ("what is your name :")
+age = input("what is your age:")
+place=input("what is your residence:")
+dob  =input("enter your date of birth:")
+print("how are you " + name + "  Age: "+ age + "residence:"+ place +"dob:" +dob)
+print("hello"+name)
+height=float(input("what your height:"))
+print("you are"+ str(height) + "cm")
+print(" you are" + str(height/2.54)+ "inchestall")
+weight=float(input("what is your weight:"))
+print("you are" + str(weight)+ "kg" )
+

155a2e29587da6bb99606589d9cec5c38070b2b3
jaganraj06 <jaganrajmurugesan06@gmail.com>
2024-04-26 17:21:04 +0530

    python learning 04

---
+++ pythonlearning05.py
@@ -0,0 +1,28 @@
+age=int(input("enter your age:"))
+if age>=18:
+    print("able vote")
+else:
+    print("notable to vote")
+
+print("don't miss to vote it is your chance to decide democracy")
+
+
+#elif ladder
+percentage=int(input("enter your mark:"))
+if percentage>=90:
+    print("you qualified in first class")
+
+elif percentage>=80:
+    print("you qualified in second class")
+
+elif percentage>=70:
+    print("you qulified in third class")
+
+elif percentage>=60:
+    print("you qulified in fourth class")
+
+elif percentage>=50:
+    print("you qulified in last class")
+
+else:
+    print("not qulified for this job")
